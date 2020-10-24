2cae432c2ac2ab0fb3b71cefd75d5579b682ec1a
Moti Elmakies <motiphone2003@gmail.com>
2020-10-25 01:05:40 +0200

    progress

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const request = require('request');
+const chalk = require('chalk');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=2fa976f7cbd583126c5f1c4dafb17713&query=%22tel%20aviv%22';
+  'http://api.weatherstack.com/current?access_key=2fa976f7cbd583126c5f1c4dafb17713&query=tel aviv&unites=m';
 
 request({ url: url, json: true }, (error, response) => {
-  const data = response.body;
-  console.log(data.current);
+  const data = response.body.current;
+  console.log(
+    chalk.yellow.inverse.bold(
+      `it is currently ${data.temperature} degress out. It feels like ${data.feelslike} degress out.`
+    )
+  );
+
+  console.log(`weather_descriptions: ${data.weather_descriptions[0]}`);
 });
